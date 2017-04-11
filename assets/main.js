adb2e37520707f2501c3956dfd83d06b76988965
emran <emranbd77@gmail.com>
2017-04-11 16:44:30 +0600

    add file

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#bar1').barfiller({
+      barColor:"#000",
+      //tooltip:true,
+      //duration:1000,
+    //  animateOnResize:true,
+    //  symbol:"%",
+    });
+    $('#bar2').barfiller({
+      barColor:"rgb(51, 26, 33)",
+    });
+    $('#bar3').barfiller({
+      barColor:"rgb(51, 26, 30)",
+    });
+    $('#bar4').barfiller({
+      barColor:"rgb(51, 26, 30)",
+    });
+    $('#bar5').barfiller({
+      barColor:"rgb(51, 26, 30)",
+    });
+});
